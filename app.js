041d75c56369aefb1e48b0a5cf7dd4d80799dba8
sir confy <csirconfy@gmail.com>
2021-05-26 08:56:14 +0100

    my portfolio

---
+++ app.js
@@ -0,0 +1,7 @@
+const navItemContainer = document.getElementById("nav-item-wrapper");
+
+
+// function that opens and close the nav menubar
+function forMenu() {
+    navItemContainer.classList.toggle("open-nav-items")
+}
